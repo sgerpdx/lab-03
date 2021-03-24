fc8a76cd3214322b4ec3d5e3802a9d0206cb74c1
SamuelHGerber <henrylightfoot@gmail.com>
2021-03-24 16:40:37 -0700

    .get endpoint set up but not working yet

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,6 +2,7 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const Order = require('../lib/models/Order');
 
 jest.mock('twilio', () => () => ({
   messages: {
@@ -14,14 +15,17 @@ describe('03_separation-of-concerns-demo routes', () => {
     return setup(pool);
   });
 
+  beforeEach(async () => {
+    await Order.insert({ quantity: 40 });
+  });
+
   it('creates a new order in our database and sends a text message', () => {
     return request(app)
       .post('/api/v1/orders')
       .send({ quantity: 10 })
       .then((res) => {
-        // expect(createMessage).toHaveBeenCalledTimes(1);
         expect(res.body).toEqual({
-          id: '1',
+          id: '2',
           quantity: 10,
         });
       });
@@ -30,13 +34,13 @@ describe('03_separation-of-concerns-demo routes', () => {
   it('gets all orders in our database and sends a text message', () => {
     return request(app)
       .get('/api/v1/orders')
-      // .send({ quantity: 10 })
       .then((res) => {
-        // expect(createMessage).toHaveBeenCalledTimes(1);
-        expect(res.body).toEqual({
-          id: '1',
-          quantity: 10,
-        });
+        expect(res.body).toEqual([
+          {
+            id: '1',
+            quantity: 40,
+          },
+        ]);
       });
   });
 
